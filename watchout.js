47f445342d81776b0d24471e35aff49f99643b12
sunyang730 <sunyang730@gmail.com>
2015-02-13 17:14:34 -0800

    - Change div and span to svg and circle - Add attributes for circle and svg - Start Current score updates

--- watchout.js
+++ watchout.js
@@ -1,28 +1,32 @@
 //** Create container for the circle.
-var container = d3.select('body').append('div')
-    .attr('class', 'container')
+var container = d3.select('body').append('svg')
+                  .attr('width', 250 + 'px')
+                  .attr('height', 250 + 'px');
 
 // ** Create/Update circles
 var updateCirclePos = function(circlePos, data){
-  var circle = container.selectAll('span')
+  var circle = container.selectAll('circle')
       .data(data);
   //** Update circle position.
   circle.transition().duration(500)
-      .style('top', function(d,i){
+      .attr('r', '5')
+      .attr('fill', 'red')
+      .attr('cx', function(d,i){
         return circlePos[0][i] + 'px';
       })
-      .style('left', function(d,i){
+      .attr('cy', function(d,i){
         return circlePos[1][i] + 'px';
       });
 
   //** Create circle at random position.
-  circle.enter().append('span')
-      .attr('class', 'circle')
-      .style('top', function(d,i){
-        return circlePos[0][i] + 'px';
+  circle.enter().append('circle')
+      .attr('r', '5')
+      .attr('fill', 'black')
+      .attr('cx', function(d,i){
+        return circlePos[0][i] + 'em';
       })
-      .style('left', function(d,i){
-        return circlePos[1][i] + 'px';
+      .attr('cy', function(d,i){
+        return circlePos[1][i] + 'em';
       });
 
 
@@ -39,9 +43,13 @@ var randomNum = function(max){
 var circlePos = function(value){
   var list = [];
   for(var i = 0; i < value; i++){
-    list.push(randomNum(190));
+    list.push(randomNum(240));
   }
   return list;
+};
+// ** update current score
+var currentScore = function(value){
+
 };
 
 // ** Initialize the first set of circles
@@ -57,7 +65,6 @@ setInterval(function(){
   circleArray.push(circlePos(10));
   circleArray.push(circlePos(10));
   updateCirclePos(circleArray, Array(10));
-
 }, 1500);
 
 
