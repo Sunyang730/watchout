e79ffece48bd55b6c1f8333ede49e687b24ddd1b
sunyang730 <sunyang730@gmail.com>
2015-02-14 14:20:50 -0800

    - Fix: game board to center. - Add: High score will refresh when there is a new score. - Need Fix: Collisions will keep increasing no matter what.

--- watchout.js
+++ watchout.js
@@ -1,5 +1,9 @@
 var height = 250;
 var width = 250;
+var collisions = 0;
+var circleArray = [];
+var score = 0;
+var currentScore = 0;
 
 
 
@@ -38,10 +42,10 @@ var updateCirclePos = function(circlePos, data){
       .attr('r', '5')
       .attr('fill', 'red')
       .attr('cx', function(d,i){
-        return (circlePos[0][i]) + 'px';
+        return (circlePos[0][i]);
       })
       .attr('cy', function(d,i){
-        return (circlePos[1][i]) + 'px';
+        return (circlePos[1][i]);
       });
 
   //** Create circle at random position.
@@ -50,10 +54,10 @@ var updateCirclePos = function(circlePos, data){
       .attr('r', '5')
       .attr('fill', 'red')
       .attr('cx', function(d,i){
-        return (circlePos[0][i]) + 'px';
+        return (circlePos[0][i]);
       })
       .attr('cy', function(d,i){
-        return (circlePos[1][i]) + 'px';
+        return (circlePos[1][i]);
       });
 
 
@@ -81,19 +85,46 @@ var circlePos = function(value){
   return list;
 };
 
-//** drag fn for playerNode
+// ** Collision detection
+
+var collision = function(collisions){
+  var edx = 0;
+  var edy = 0;
+
+  container.selectAll('.enemyNode').each(function(d){
+    if(currentScore < score){
+      currentScore = score;
+    }
+    if(Math.abs(d3.select(this).attr('cx') - playerNode.attr('cx')) <10 && Math.abs(d3.select(this).attr('cy') - playerNode.attr('cy'))<10){
+      d3.select('.collisions').select('span').text(collisions);
+      d3.select('.high').select('span').text(currentScore);
+      score =0;
+      d3.select('.current').select('span').text(score);
+
+    }
+
+  });
+
+  return collisions+1;
+
+};
+//** drag fn for playerNode also check for collision if cx and cy match
 var dragged = function(d){
   playerNode.attr('cx', d3.event.x)
     .attr('cy', d3.event.y);
-
 };
 
+// ** time to detect collision
+setInterval(function(){
+  collisions += collision(collisions);
+}, 1000);
+
 // ** Initialize the first set of circles
-var circleArray = [];
-var score = 0;
 circleArray.push(circlePos(10));
 circleArray.push(circlePos(10));
 updateCirclePos(circleArray, Array(10));
+
+
 
 
 //** Generate the next set of circles
